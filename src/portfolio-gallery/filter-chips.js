22b2ade1d92e47d5c7b6043f7302db232e4ce67d
RaphaelPosch <raphael.posch@kabelplus.at>
2022-10-03 20:56:40 +0200

    add 3d projects

---
+++ src/portfolio-gallery/filter-chips.js
@@ -0,0 +1,8 @@
+const designFilter = document.querySelectorAll('ul li');
+const imgItem = document.querySelectorAll('.project');
+
+designFilter.forEach(li => {
+    li.onclick = function()
+})
+
+const value = 
