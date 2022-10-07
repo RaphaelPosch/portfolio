2f90a1b88511061752436c153ca184011686b4b6
Kai Wißler <kaiwissler@Kais-MacBook-Pro-2.local>
2022-10-07 19:13:29 +0200

    feat: add functionality to filter chips

--- src/portfolio-gallery/filter-chips.js
+++ src/portfolio-gallery/filter-chips.js
@@ -1,8 +1,20 @@
-const designFilter = document.querySelectorAll('ul li');
-const imgItem = document.querySelectorAll('.project');
+const designFilterCheckboxes = document.querySelectorAll('ul li input');
+const projectSections = document.querySelectorAll('.project');
 
-designFilter.forEach(li => {
-    li.onclick = function()
+const FILTER_ID_ATTRIBUTE = 'data-filter';
+const FILTER_ID_ALL = 'all';
+const HIDDEN_UTILITY_CLASS = 'hidden';
+
+designFilterCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', (event) => {
+        if(event.target.checked && event.target.id !== FILTER_ID_ALL) {
+            projectSections.forEach(section => {
+                if(section.getAttribute(FILTER_ID_ATTRIBUTE) !== event.target.id) {
+                    section.classList.add(HIDDEN_UTILITY_CLASS);
+                } else {
+                    section.classList.remove(HIDDEN_UTILITY_CLASS);
+                }
+            })
+        }
+    })
 })
-
-const value = 
