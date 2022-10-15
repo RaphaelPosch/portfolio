2bf789989c5f86a0ce3f1f8c03d92e9253c5fab9
RaphaelPosch <raphael.posch@kabelplus.at>
2022-10-15 15:37:53 +0200

    adjust copy

--- src/portfolio-gallery/filter-chips.js
+++ src/portfolio-gallery/filter-chips.js
@@ -7,13 +7,18 @@ const HIDDEN_UTILITY_CLASS = 'hidden';
 
 designFilterCheckboxes.forEach(checkbox => {
     checkbox.addEventListener('change', (event) => {
-        if(event.target.checked && event.target.id !== FILTER_ID_ALL) {
+        if(event.target.checked === true && event.target.id !== FILTER_ID_ALL) {
             projectSections.forEach(section => {
                 if(section.getAttribute(FILTER_ID_ATTRIBUTE) !== event.target.id) {
                     section.classList.add(HIDDEN_UTILITY_CLASS);
                 } else {
                     section.classList.remove(HIDDEN_UTILITY_CLASS);
                 }
+            })
+        } else {
+            projectSections.forEach(section => {
+                    section.classList.remove(HIDDEN_UTILITY_CLASS);
+                
             })
         }
     })
